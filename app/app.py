e7711a71a6e5fb20a9a92e6275c440ef76f68d3e
DanaAkp <aidnagul10@gmail.com>
2021-06-24 11:28:26 +0500

    расписание и аудитории доступно по rest api, проверка свободной аудитории перенесена в views.py

--- app/app.py
+++ app/app.py
@@ -41,15 +41,4 @@ def insert_test_data():
         db.session.commit()
 
 
-def is_room_busy(date_start, id_room) -> bool:
-    """Метод, определяющий занята ли аудитория в выбранное время.
-    Если занята, то возвращает True, иначе - False."""
-
-    presentation_at_time = Schedule.query.filter_by(id_room=int(id_room), date_start=datetime.
-                                                    strptime(date_start, '%Y-%m-%d %H:%M:%S')).first()
-    if presentation_at_time is None:
-        return False
-    return True
-
-
 from app.views import *
--- app/views.py
+++ app/views.py
@@ -55,12 +55,109 @@ def delete_presentation(presentation_id):
     db.session.delete(presentation)
     db.session.commit()
     return jsonify({'result': True})
+
+
 # endregion
 
 
 # region Room
+@app.route('/conference/api/rooms', methods=['GET'])
+def get_rooms():
+    return jsonify(json_list=[i.serialize for i in Room.query.all()])
+
+
+@app.route('/conference/api/rooms/<int:rooms_id>', methods=['GET'])
+def get_room(rooms_id):
+    room = Room.query.filter_by(id=rooms_id).first()
+    if room is None:
+        abort(404)
+    return jsonify(room.serialize)
+
+
+@app.route('/conference/api/rooms', methods=['POST'])
+def create_room():
+    if not request.json or 'name' not in request.json:
+        abort(400)
+    if Room.query.filter_by(name=request.json['name']).first() is not None:
+        abort(409, f'Room is exist.')
+    new_room = Room(name=request.json['name'])
+    db.session.add(new_room)
+    db.session.commit()
+    return jsonify(json_list=[i.serialize for i in Room.query.all()]), 201
+
+
+@app.route('/conference/api/rooms/<int:rooms_id>', methods=['PUT'])
+def update_room(rooms_id):
+    room = Room.query.filter_by(id=rooms_id).first()
+    if room is None or not request.json or 'name' not in request.json:
+        abort(400)
+    room.name = request.json['name']
+    db.session.commit()
+    return jsonify(Room.query.filter_by(id=rooms_id).first().serialize)
+
+
+@app.route('/conference/api/rooms/<int:rooms_id>', methods=['DELETE'])
+def delete_room(rooms_id):
+    room = Room.query.filter_by(id=rooms_id).first()
+    if room is None:
+        abort(404)
+    db.session.delete(room)
+    db.session.commit()
+    return jsonify({'result': True})
+
 
 # endregion
 
 
+# region Schedule
+@app.route('/conference/api/schedule', methods=['GET'])
+def get_schedule():
+    return jsonify(json_list=[i.serialize for i in Schedule.query.all()])
 
+
+@app.route('/conference/api/schedule/<int:schedule_id>', methods=['GET'])
+def get_record_in_schedule(schedule_id):
+    schedule = Schedule.query.filter_by(id=schedule_id).first()
+    if schedule is None:
+        abort(404)
+    return jsonify(schedule.serialize)
+
+
+@app.route('/conference/api/schedule', methods=['POST'])
+def create_record_in_schedule():
+    if not request.json or 'date_start' not in request.json or 'presentation_id' not in request.json or \
+            'room_id' not in request.json:
+        abort(400)
+    if Schedule.query.filter_by(room_id=request.json['room_id'],
+                                date_start=request.json['date_start']).first() is not None:
+        abort(409, f'Room is busy in this time.')
+    new_room = Room(room_id=request.json['room_id'], date_start=request.json['date_start'],
+                    presentation_id=request.json['presentation_id'])
+    db.session.add(new_room)
+    db.session.commit()
+    return jsonify(json_list=[i.serialize for i in Schedule.query.all()]), 201
+
+
+@app.route('/conference/api/schedule/<int:schedule_id>', methods=['PUT'])
+def update_schedule(schedule_id):
+    # TODO правильно ли по id обращаться к расписанию
+    schedule = Schedule.query.filter_by(id=schedule_id).first()
+    if schedule is None or not request.json or 'room_id' not in request.json or 'presentation_id' not in request.json \
+            or 'date_start' not in request.json:
+        abort(400)
+    schedule.room_id = request.json['room_id']
+    schedule.presentation_id = request.json['presentation_id']
+    schedule.date_start = request.json['date_start']
+    db.session.commit()
+    return jsonify(Schedule.query.filter_by(id=schedule_id).first().serialize)
+
+
+@app.route('/conference/api/schedule/<int:schedule_id>', methods=['DELETE'])
+def delete_record_in_schedule(schedule_id):
+    schedule = Schedule.query.filter_by(id=schedule_id).first()
+    if schedule is None:
+        abort(404)
+    db.session.delete(schedule)
+    db.session.commit()
+    return jsonify({'result': True})
+# endregion
