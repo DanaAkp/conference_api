27eaa2f1a02c9dbea68a5020463f13ed013f5024
DanaAkp <aidnagul10@gmail.com>
2021-06-26 10:46:01 +0500

    добавлен bootstrap и изменен доступ к админке

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 FLASK_ENV = os.environ.get("FLASK_ENV") or 'development'
@@ -13,16 +14,23 @@ app.static_folder = app.config['STATIC_FOLDER']
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 admin = Admin(app)
+bootstrap = Bootstrap(app)
 
 from app.models import Schedule, Room, User, Role, Presentation, generate_password_hash
 
 db.create_all()
 
-admin.add_view(ModelView(Presentation, db.session))
-admin.add_view(ModelView(Schedule, db.session))
-admin.add_view(ModelView(Room, db.session))
-admin.add_view(ModelView(Role, db.session))
-admin.add_view(ModelView(User, db.session))
+
+class MyModelView(ModelView):
+    def is_accessible(self):
+        return current_user.role_id == 1
+
+
+admin.add_view(MyModelView(Presentation, db.session))
+admin.add_view(MyModelView(Schedule, db.session))
+admin.add_view(MyModelView(Room, db.session))
+admin.add_view(MyModelView(Role, db.session))
+admin.add_view(MyModelView(User, db.session))
 
 
 @login_manager.user_loader
@@ -36,9 +44,9 @@ def insert_test_data():
     db.create_all()
     roles = [Role(name='admin'), Role(name='presenter'), Role(name='listener')]
 
-    users = [User(name='admin', role_id=1, password_hash=generate_password_hash('password')),
-             User(name='presenter', role_id=2, password_hash=generate_password_hash('password')),
-             User(name='listener', role_id=3, password_hash=generate_password_hash('password'))]
+    users = [User(name='administrator', role_id=1, password_hash=generate_password_hash('password')),
+             User(name='user presenter', role_id=2, password_hash=generate_password_hash('password')),
+             User(name='user listener', role_id=3, password_hash=generate_password_hash('password'))]
 
     presentations = [Presentation(name='presentation 1', text='presentation text'),
                      Presentation(name='presentation 2', text='presentation text'),
